a5cb1ddd2cf670554a90b68e5bb248a12a66873a
G@&R <gabrielailarreta@gmail.com>
2020-11-26 00:15:42 -0300

    Update streamlit_csv_example.py

--- streamlit_csv_example.py
+++ streamlit_csv_example.py
@@ -22,7 +22,29 @@ df[df['Country_Region'] == country]
 '# Recomendaciones'
 '# ....'
 
+data = pd.DataFrame({
+    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+})
 
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(data['lat']), np.average(data['lon']))
+
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 4
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': data,
+                'radiusScale': 250,
+   'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
 
 
 
