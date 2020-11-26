f362550e5a8334559641a25ef39e93f5a699ed8f
G@&R <gabrielailarreta@gmail.com>
2020-11-26 00:58:24 -0300

    Update streamlit_csv_example.py

--- streamlit_csv_example.py
+++ streamlit_csv_example.py
@@ -25,18 +25,9 @@ df[df['Country_Region'] == country]
 '# ....'
 
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
-
-
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-
-import plotly.express as px
 
 fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
                         color_discrete_sequence=["fuchsia"], zoom=3, height=300)
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-
-
