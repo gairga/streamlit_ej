49e9bd95ba06c542aab059a1208bf4ee77e86db1
G@&R <gabrielailarreta@gmail.com>
2020-11-26 00:25:00 -0300

    Update streamlit_csv_example.py

--- streamlit_csv_example.py
+++ streamlit_csv_example.py
@@ -1,7 +1,7 @@
 import dash
 import streamlit as st
 import pandas as pd
-import dash_html_components as html
+
 
 @st.cache
 def get_data():
@@ -50,16 +50,12 @@ st.deck_gl_chart(
 
 
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(
-[html.H1("Hello World")]
-)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
+server = app.server
+app.config.suppress_callback_exceptions = True
 
 
 
